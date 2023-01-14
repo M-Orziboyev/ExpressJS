0b79e740fca5d6c0df643a52b2faad524bf17102
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-01-14 20:54:00 +0500

    This commit is a lesson that connect mongoDB to NodeJS file

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import express from "express";
-import {engine, create} from 'express-handlebars';
+import express from "express"
+import {engine, create} from 'express-handlebars'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+//Routes
 import AuthRouter from './routes/auth.js'
 import ProductsRouter from './routes/products.js'
+
+dotenv.config()
 
 const app = express();
 
@@ -10,13 +16,23 @@ const hbs = create({defaultLayout: 'main', extname: 'hbs',})
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
+mongoose.set('strictQuery', false)
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
+app.use(express.json());
 
 app.use(AuthRouter)
 app.use(ProductsRouter)
 
+mongoose.connect(process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    },
+    () => console.log("Mongo DB connection established")
+)
 
 
 const PORT = process.env.PORT || 4100
