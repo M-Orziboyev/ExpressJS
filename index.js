ecc6b9c46cd5319a4076fa8e1845102d20468df6
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2023-01-17 19:53:56 +0500

    Model mongoose User

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-import express from "express"
-import {engine, create} from 'express-handlebars'
-import mongoose from 'mongoose'
-import * as dotenv from 'dotenv'
+import express from "express";
+import { engine, create } from "express-handlebars";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
 //Routes
-import AuthRouter from './routes/auth.js'
-import ProductsRouter from './routes/products.js'
+import AuthRouter from "./routes/auth.js";
+import ProductsRouter from "./routes/products.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-const hbs = create({defaultLayout: 'main', extname: 'hbs',})
+const hbs = create({ defaultLayout: "main", extname: "hbs" });
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', './views');
-mongoose.set('strictQuery', false)
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(express.json());
 
-app.use(AuthRouter)
-app.use(ProductsRouter)
+app.use(AuthRouter);
+app.use(ProductsRouter);
 
-mongoose.connect(process.env.MONGO_URI,
-    {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    },
-    () => console.log("Mongo DB connection established")
-)
+const startApp = () => {
+	try {
+		mongoose.set('strictQuery', false)
+		mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, () => console.log('Mongo DB connected'))
 
+		const PORT = process.env.PORT || 4100
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+	} catch (error) {
+		console.log(error)
+	}
+}
 
-const PORT = process.env.PORT || 4100
-app.listen(4100, (err, res) => {console.log(`Server is running ${PORT}`);})
+startApp()     
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import User from '../models/User.js'
 
 const router = Router();
 
@@ -18,8 +19,15 @@ router.post('/login', (req, res) => {
     console.log(req.body);
     res.redirect('/')
 })
-router.post('/register', (req, res) => {
-    console.log(req.body);
+router.post('/register', async (req, res) => {
+	const userData = {
+		firstName: req.body.firstname,
+		lastName: req.body.lastname,
+		email: req.body.email,
+		password: req.body.password,
+	}
+	const user = await User.create(userData)
+    console.log(user);
     res.redirect('/')
 })
 
